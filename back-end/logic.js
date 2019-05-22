26237c248ebdbc93299dfa4b1344b9d5357cd7a6
apizarras <angelalee.p@gmail.com>
2019-05-21 20:47:05 -0500

    post logic for input form

--- back-end/logic.js
+++ back-end/logic.js
@@ -41,20 +41,23 @@ app.get("/reservation-view.html", function(request, response) {
     response.sendFile(path.join(__dirname, "../front-end/reservation-view.html"));
 });
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+$("submit-btn").on("click", function() {
+    app.post("/api/reservations", function(req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var newReservations = req.body;
+      
+        console.log(newReservations);
+      
+        // We then add the json the user sent to the character array
+        reservations.push(newReservations);
+      
+        // We then display the JSON to the users
+        res.json(newReservations);
+      });
+});
+  
 
-  app.post("/api/reservations", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newReservations = req.body;
-  
-    console.log(newReservations);
-  
-    // We then add the json the user sent to the character array
-    reservations.push(newReservations);
-  
-    // We then display the JSON to the users
-    res.json(newReservations);
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
   });
